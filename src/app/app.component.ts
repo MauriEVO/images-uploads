d8f7d05af9c9864d2900fe8b711dbea2fb33914c
mauri villafuerte <maury.villafuerte@gmail.com>
2024-07-24 09:59:50 -0500

    base

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ImageUploadComponent } from "./components/image-upload/image-upload.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ImageUploadComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/components/image-upload/image-upload.component.ts
@@ -0,0 +1,58 @@
+import { Component, inject } from '@angular/core';
+import { FakeImageUploadService } from '../../services/fake-image-upload.service';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-image-upload',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './image-upload.component.html',
+  styleUrl: './image-upload.component.scss'
+})
+
+export class ImageUploadComponent {
+  fakeImageUploadService = inject(FakeImageUploadService);
+  uploading = false;
+  selectedImages!: FileList;
+  imagesUrl: Array<string> = [];
+
+  onImageSelected(event: Event): void {
+    const inputElement = event.target as HTMLInputElement;
+
+    if (inputElement?.files && inputElement.files.length > 0) {
+      this.selectedImages = inputElement.files;
+    }
+  }
+
+  upload(): void {
+    if (this.selectedImages) {
+      this.uploadFiles(this.selectedImages);
+    }
+  }
+
+  private uploadFiles(images: FileList): void {
+    this.uploading = true;
+
+    for (let index = 0; index < images.length; index++) {
+      const element = images[index];
+
+      this.fakeImageUploadService.uploadImage(element).subscribe((p) => {
+        this.imagesUrl.push(p);
+
+        this.uploading = false;
+      });
+    }
+  }
+
+  onDrop(event: DragEvent): void {
+    event.preventDefault();
+
+    if (event?.dataTransfer?.files) {
+      this.uploadFiles(event.dataTransfer.files);
+    }
+  }
+
+  onDragOver(event: DragEvent): void {
+    event.preventDefault();
+  }
+}
